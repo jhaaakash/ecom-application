eb83327f642d8a277b224739e7fcf0925a214576
jhaaakash <aakashkumarjha295@gmail.com>
2025-06-03 00:49:31 +0530

    Product Controller Service Repository request response

--- src/main/java/com/app/ecom/controller/UserController.java
+++ src/main/java/com/app/ecom/controller/UserController.java
@@ -1,12 +1,14 @@
-package com.app.ecom;
+package com.app.ecom.controller;
 
+import com.app.ecom.dto.UserRequest;
+import com.app.ecom.dto.UserResponse;
+import com.app.ecom.model.User;
+import com.app.ecom.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,29 +20,29 @@ public class UserController {
 
     @GetMapping("/api/users")
     //Fetching All user
-    public ResponseEntity<List<User>> getAllUsers(){
+    public ResponseEntity<List<UserResponse>> getAllUsers(){
            //return  ResponseEntity.ok(userService.fetchAllUsers());
        return  new ResponseEntity<>(userService.fetchAllUsers(), HttpStatus.OK);
     }
 
     //Fetching an Specific user
     @GetMapping("/api/users/{id}")
-    public ResponseEntity<User> getUsers(@PathVariable Long id){
+    public ResponseEntity<UserResponse> getUsers(@PathVariable Long id){
         return userService.fetchUsers(id).map(ResponseEntity::ok).orElseGet(()-> ResponseEntity.notFound().build());
     }
 
     //Creating a new User
     @PostMapping(value = "/api/user")
-    public ResponseEntity<String> createUser(@RequestBody User user) {
-            userService.addUser(user);
-            return ResponseEntity.ok("User:"+ user.getFirstName()+ " added Successfully");
+    public ResponseEntity<String> createUser(@RequestBody UserRequest userRequest) {
+            userService.addUser(userRequest);
+            return ResponseEntity.ok("User: added Successfully");
     }
 
     //Updating an User
     @PutMapping("/api/user/{id}")
-    public ResponseEntity<String> updateUser(@PathVariable Long id , @RequestBody User updatedUser)
+    public ResponseEntity<String> updateUser(@PathVariable Long id , @RequestBody UserRequest updatedUserRequest)
     {
-        boolean updated = userService.updteUser(id, updatedUser);
+        boolean updated = userService.updteUser(id, updatedUserRequest);
         if(updated)
         {
             return ResponseEntity.ok("User updated Successfully");
